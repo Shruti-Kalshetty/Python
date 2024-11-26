e917b15bb77366e1f90ff7607cad7cdde2b7d8a6
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-11-26 22:25:58 +0530

    Add files via upload

---
+++ TrafficLight.py
@@ -0,0 +1,14 @@
+
+
+light=input("Traffic Light : ")
+if(light == "Red"):
+    print("Stop")
+
+elif (light == "Yellow") :
+    print("Ready")
+
+elif (light == "Green") :
+    print("Go")
+
+else:
+     print("Light is broken")
