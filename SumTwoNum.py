ccf4d8dd87ea55dd6b8aabe5b2a22c381bce58cf
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-11-26 22:12:08 +0530

    Add files via upload

---
+++ SumTwoNum.py
@@ -0,0 +1,47 @@
+"""a=2
+b=3
+sum=a+b
+print(sum)"""
+
+"""name = input("name : ")
+age=int(input("age : "))
+price=float(input("price :"))
+print("My name is ", name , "and I am ", age, "years old")"""
+
+"""light=input("traffic light :")
+if(light=="red") :
+    print("stop")
+elif(light=="yellow"):
+    print ("look")
+elif(light=="green"):
+     print("go")
+else:
+     print("light is broken")"""
+
+"""A=int(input("A : "))
+G=input("M/F : ")
+if((A==1 or A==2) and G=="M"):
+    print("Fee is 100")
+elif((A==3 or A==4) and G =="F"):
+    print("Fee is 200")
+elif((A==5 and G == "M")):
+    print("Fee is 300")
+else:
+    print("no fee")"""
+
+"""food=input("food :")
+eat="yes" if food=="cake" else "no"
+print(eat)"""
+
+"""food=input("food :")
+print("sweet") if food=="cake" or food=="jalebi" else print("not sweet")"""
+
+"""age=int (input("age :"))
+vote=("yes", "no") [age<=18] 
+print(vote)"""
+
+"""sal=float(input("salary :"))
+tax=sal*(0.1, 0.2) [sal>=50000]
+print(tax)"""
+
+
